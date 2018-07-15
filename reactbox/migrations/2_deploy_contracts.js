3e816a7e8215b195c181fb92ee1734f582ba660d
vinsonly <vinsonly@live.ca>
2018-07-14 23:36:01 -0700

    Modifed to work on my computer, there was bug where the contract definition was differnet from the contract name

--- reactbox/migrations/2_deploy_contracts.js
+++ reactbox/migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var MyContract = artifacts.require("./MyContract.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(MyContract);
 };
