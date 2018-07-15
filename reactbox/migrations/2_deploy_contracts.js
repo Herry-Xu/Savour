e910f79e86c1bf0a862a33e4ce0e1beb25e99f88
Herry-Xu <herryxu123@gmail.com>
2018-07-14 21:19:48 -0700

    ok

--- reactbox/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var MyContract = artifacts.require("./MyContract.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(MyContract);
-};
