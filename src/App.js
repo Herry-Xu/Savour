c70428c4d4bbe4aa6a7eff20505f439e3849478d
vinsonly <vinsonly@live.ca>
2018-07-14 19:19:28 -0700

    blah

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import logo from './logo.svg';
 import './App.sass';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props){
+    super(props)
+    this.state = {
+      lastWinner: 0,
+      timer: 0
+    }
+  }
+  voteNumber(number){
+      console.log(number)
+    }
+  render(){
+      return (
+          <div className="main-container">
+            <h1>Bet for your best number and win huge amounts of Ether</h1>
+            <div className="block">
+                          <h4>Timer:</h4> &nbsp;
+                          <span ref="timer"> {this.state.timer}</span>
+                      </div>
+            <div className="block">
+                          <h4>Last winner:</h4> &nbsp;
+                          <span ref="last-winner">{this.state.lastWinner}</span>
+                      </div>
+            <hr/>
+            <h2>Vote for the next number</h2>
+            <ul>
+                <li onClick={() => {this.voteNumber(1)}}>1</li>
+                <li onClick={() => {this.voteNumber(2)}}>2</li>
+                <li onClick={() => {this.voteNumber(3)}}>3</li>
+                <li onClick={() => {this.voteNumber(4)}}>4</li>
+                <li onClick={() => {this.voteNumber(5)}}>5</li>
+                <li onClick={() => {this.voteNumber(6)}}>6</li>
+                <li onClick={() => {this.voteNumber(7)}}>7</li>
+                <li onClick={() => {this.voteNumber(8)}}>8</li>
+                <li onClick={() => {this.voteNumber(9)}}>9</li>
+                <li onClick={() => {this.voteNumber(10)}}>10</li>
+            </ul>
+          </div>
+      )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Web3 from 'web3'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker';
 
-import routes from './routes';
+class App extends React.Component {
+    constructor(props){
+       super(props)
+       this.state = {
+          lastWinner: 0,
+          timer: 0
+       }
 
-import './styles/global.sass';
-import './favicon.ico';
+       var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+       window.web3 = web3;
 
+       this.web3 = web3;
+       window.Web3 = Web3;
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+    }
+ voteNumber(number){
+       console.log(number)
+    }
+ render(){
+       return (
+          <div className="main-container">
+             <h1>Bet for your best number and win huge amounts of Ether</h1>
+ <div className="block">
+                <h4>Timer:</h4> &nbsp;
+                <span ref="timer"> {this.state.timer}</span>
+             </div>
+ <div className="block">
+                <h4>Last winner:</h4> &nbsp;
+                <span ref="last-winner">{this.state.lastWinner}</span>
+             </div>
+ <hr/>
+ <h2>Vote for the next number</h2>
+             <ul>
+                <li onClick={() => {this.voteNumber(1)}}>1</li>
+                <li onClick={() => {this.voteNumber(2)}}>2</li>
+                <li onClick={() => {this.voteNumber(3)}}>3</li>
+                <li onClick={() => {this.voteNumber(4)}}>4</li>
+                <li onClick={() => {this.voteNumber(5)}}>5</li>
+                <li onClick={() => {this.voteNumber(6)}}>6</li>
+                <li onClick={() => {this.voteNumber(7)}}>7</li>
+                <li onClick={() => {this.voteNumber(8)}}>8</li>
+                <li onClick={() => {this.voteNumber(9)}}>9</li>
+                <li onClick={() => {this.voteNumber(10)}}>10</li>
+             </ul>
+          </div>
+       )
+    }
+ }
+
+ReactDOM.render(<App />, document.getElementById('app'));
+registerServiceWorker();
